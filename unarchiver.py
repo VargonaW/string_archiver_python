1652f6ca4af400a1eebb284d6e7fd107796c433a
VargonaW <95219207+VargonaW@users.noreply.github.com>
2024-01-22 14:12:27 +0500

    Update unarchiver.py

--- unarchiver.py
+++ unarchiver.py
@@ -1,9 +1,9 @@
-def unarchiver(string:str):
-    result:str = ""
-    numbers:str = "0123456789"
-    first:str = string[0]
-    counter:str = ""
-    second:str = ""
+def unarchiver(string:str) -> str:
+    result: str = ""
+    numbers: str = "0123456789"
+    first: str = string[0]
+    counter: str = ""
+    second: str = ""
     for i in range(1, len(string)):
         second = string[i]
         if second in numbers:
